a506ede190dd464d57c4f51712724c21e99c1985
iramamy <mamy@aims.ac.za>
2024-08-04 23:18:05 +0200

    create super user

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -28,6 +28,20 @@ class ModelTest(TestCase):
             ["TEST4@EXAMPLE.com", "TEST4@example.com"],
         ]
 
-        for email, expected in sample_emails:
+        for email, expected_email in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
-            self.assertEqual(user.email, expected)
+            self.assertEqual(user.email, expected_email)
+
+    def test_new_user_without_email_raises_error(self):
+        """Raises error when user does not provide email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
+
+    def test_create_super_user(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "test123"
+        )  # noqa
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
